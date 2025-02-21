6b8c11694607f36ab57abf92bed1a0524dfda3cd
Rajneesh Kumar <rajneesh.coder@gmail.com>
2025-02-21 18:46:10 +0530

    added outlet to admin

--- frontend/src/admin/Admin.jsx
+++ frontend/src/admin/Admin.jsx
@@ -1,19 +1,15 @@
 import {useSelector} from "react-redux"
+import { Outlet } from "react-router-dom"
 
 export default function Admin(){
     const profile = useSelector(s=>s.profile)
 
      return (
         <>
-            <main className="Admin_container size-full max-md:pt-20 pt-10 px-8 min-h-screen">
-                
+            <main className="Admin_container size-full max-md:pt-20 pt-10 px-8 min-h-screen ">
+            <div className="px-4 py-2 bg-green-300 rounded-md"> as deadline is near, I am not adding styles in admin </div>
 
-                <p>
-                    {profile.data.role}
-                </p>
-                <p>
-                    {profile.data.username}
-                </p>
+                <Outlet/>
                 
 
             </main>
