2af72400543fd348789a100f0b5a801941334e5a
Rajneesh Kumar <rajneesh.coder@gmail.com>
2025-02-21 13:02:13 +0530

    update slice

--- frontend/src/states/store.js
+++ frontend/src/states/store.js
@@ -1,13 +1,17 @@
 import {configureStore} from "@reduxjs/toolkit"
 
-// slices
+// AUTH slices
 import token from "./token_slice"
 import profile_slice  from "./userInfo_slice"
+
+// user slices
+import not_claimed_apps from "./UserStates/apps_not_claimed"
 
 // store
 export default configureStore({
     reducer : {
         token : token,
         profile : profile_slice,
+        pending_apps : not_claimed_apps,
     }
 })
