d61bb00fcc4a45fba0bbc8843a7db19488f9a112
Rajneesh Kumar <rajneesh.coder@gmail.com>
2025-02-21 14:55:21 +0530

    added history sidebar option & icon

--- frontend/src/states/SidebarItems.jsx
+++ frontend/src/states/SidebarItems.jsx
@@ -2,17 +2,19 @@ import {
     HomeIcon,
     User2Icon,
     LucideAppWindow,
-    TableOfContentsIcon
+    ServerIcon,
+    HistoryIcon,
 } from "lucide-react"
 
 export const SidebarItems = () => {
     const data = [
             { title: "admin", link: "/admin", icon: <LucideAppWindow/>, allowed_roles: ['admin']},
 
-            { title: "Task", link: "/", icon: <TableOfContentsIcon/>, allowed_roles: ['user', 'admin'] },
+            { title: "Task", link: "/", icon: <ServerIcon/>, allowed_roles: ['user', 'admin'] },
 
             { title: "profile", link: "profile", icon: <User2Icon/>, allowed_roles: ['user', 'admin'] },
-
+            
+            { title: "history", link: "history", icon: <HistoryIcon/>, allowed_roles: ['user', 'admin'] },
         ]
     return data;
 }
