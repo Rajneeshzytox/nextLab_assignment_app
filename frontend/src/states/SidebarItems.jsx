0cac2bdc3161e9b84a5b42f7d533b5657d0bf7e1
Rajneesh Kumar <rajneesh.coder@gmail.com>
2025-02-24 15:10:09 +0530

    minor fix: seperated icons admin/user

--- frontend/src/states/SidebarItems.jsx
+++ frontend/src/states/SidebarItems.jsx
@@ -9,19 +9,17 @@ import {
 
 export const SidebarItems = () => {
     const data = [
+
+        // ADMIN 
             { title: "admin", link: "/admin", icon: <LucideAppWindow/>, allowed_roles: ['admin']},
-
-            { title: "Task", link: "/", icon: <ServerIcon/>, allowed_roles: ['user', 'admin'] },
-
-            { title: "profile", link: "profile", icon: <User2Icon/>, allowed_roles: ['user', 'admin'] },
-            
-            { title: "history", link: "history", icon: <HistoryIcon/>, allowed_roles: ['user', 'admin'] },
-
-            { title: "categories", link: "/admin/categories", icon: <ChartNoAxesGanttIcon/>, allowed_roles: ['admin'] },
-            
-            { title: "Sub Categories", link: "/admin/sub-categories", icon: <ChartNoAxesGanttIcon/>, allowed_roles: ['admin'] },
-
             { title: "Apps", link: "/admin/apps", icon: <AirplayIcon/>, allowed_roles: ['admin'] },
+            { title: "categories", link: "/admin/categories", icon: <ChartNoAxesGanttIcon/>, allowed_roles: ['admin'] },
+            { title: "Sub Categories", link: "/admin/sub-categories", icon: <ChartNoAxesGanttIcon/>, allowed_roles: ['admin'] },
+            
+        // User
+            { title: "Task", link: "/", icon: <ServerIcon/>, allowed_roles: ['user', 'admin'] },
+            { title: "history", link: "history", icon: <HistoryIcon/>, allowed_roles: ['user', 'admin'] },
+            { title: "profile", link: "profile", icon: <User2Icon/>, allowed_roles: ['user', 'admin'] },
         ]
     return data;
 }
